4082ee8ca2673a0539620ae4ae4dab40471bc033
zsxneil <zsxneil@hotmail.com>
2018-01-05 20:04:00 +0800

    冲突解决

--- src/main/java/com/my/App.java
+++ src/main/java/com/my/App.java
@@ -1,13 +1,22 @@
 package com.my;
 
+import io.vertx.core.Vertx;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        Vertx vertx;
+
+        //int port = 8081;
+        vertx = Vertx.vertx();
+        //ServerSocket serverSocket = new ServerSocket(0);
+        //port = serverSocket.getLocalPort();
+        //serverSocket.close();
+        //DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("http.port",port));
+        vertx.deployVerticle(OKHttpVerticle.class.getName());
     }
 }
