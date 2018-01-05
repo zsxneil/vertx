d65af1e79693131205194116754df62ace140ac6
zsxneil <zsxneil@hotmail.com>
2018-01-05 11:16:27 +0800

    okhttp测试的服务端

--- src/main/java/com/my/App.java
+++ src/main/java/com/my/App.java
@@ -1,13 +1,29 @@
 package com.my;
 
+import com.my.learn.MyFirstVerticle;
+import com.my.learn.OKHttpVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        Vertx vertx;
+
+        //int port = 8081;
+        vertx = Vertx.vertx();
+        //ServerSocket serverSocket = new ServerSocket(0);
+        //port = serverSocket.getLocalPort();
+        //serverSocket.close();
+        //DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("http.port",port));
+        vertx.deployVerticle(OKHttpVerticle.class.getName());
     }
 }
